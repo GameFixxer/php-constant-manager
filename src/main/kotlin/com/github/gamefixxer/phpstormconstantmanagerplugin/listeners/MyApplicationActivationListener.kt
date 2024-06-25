a027951f2d55f870e873089b3eafdff9b09ad4d2
GitHub Action <action@github.com>
2024-06-25 12:47:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/gamefixxer/phpstormconstantmanagerplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gamefixxer/phpstormconstantmanagerplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gamefixxer.phpstormconstantmanagerplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/gamefixxer/phpstormconstantmanagerplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gamefixxer/phpstormconstantmanagerplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.gamefixxer.phpstormconstantmanagerplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gamefixxer.phpstormconstantmanagerplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
